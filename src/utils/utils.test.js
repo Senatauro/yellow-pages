f489508f85077f719fbea0fa749fb0b9f82be1f9
Senatauro <cgsste@hotmail.com>
2022-07-10 17:30:42 -0300

    minor update
    Updating the test for the regex function in utils

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -4,7 +4,7 @@ import {parseSearchInfo} from "./utils";
 // RegEx testing //
 ///////////////////
 // This test will pass if the parseSearchInfo function works correctly and returns the correct object
-describe('Test true expression on the RegEx from utils', () => {
+describe('Test expression on the RegEx from utils', () => {
     describe("Phone tests", () => {
         test("Testing for full number", () => {
             let searchInput = "(053) 858-9246"
@@ -21,14 +21,12 @@ describe('Test true expression on the RegEx from utils', () => {
         
             searchInput = "053858-9246"
             searchResult = parseSearchInfo(searchInput)
-            console.log(searchResult)
             expect(searchResult.name).toBe("")
             expect(searchResult.phone).toBe("0538589246")
             expect(searchResult.age).toBe("")
 
             searchInput = "0538589246"
             searchResult = parseSearchInfo(searchInput)
-            console.log(searchResult)
             expect(searchResult.name).toBe("")
             expect(searchResult.phone).toBe("0538589246")
             expect(searchResult.age).toBe("")
@@ -38,7 +36,6 @@ describe('Test true expression on the RegEx from utils', () => {
         test("Testing for half number", () => {
             let searchInput = "(053) 858"
             let searchResult = parseSearchInfo(searchInput)
-            console.log(searchResult)
             expect(searchResult.name).toBe("")
             expect(searchResult.phone).toBe("053858")
             expect(searchResult.age).toBe("")
@@ -51,17 +48,55 @@ describe('Test true expression on the RegEx from utils', () => {
         
             searchInput = "053858"
             searchResult = parseSearchInfo(searchInput)
-            console.log(searchResult)
             expect(searchResult.name).toBe("")
             expect(searchResult.phone).toBe("053858")
             expect(searchResult.age).toBe("")
 
             searchInput = "053858"
             searchResult = parseSearchInfo(searchInput)
-            console.log(searchResult)
             expect(searchResult.name).toBe("")
             expect(searchResult.phone).toBe("053858")
             expect(searchResult.age).toBe("")
         })
-    })    
+
+        test("Possible user phone entries", () => {
+            let searchInput = "077"
+            let searchResult = parseSearchInfo(searchInput)
+            expect(searchResult.name).toBe("")
+            expect(searchResult.phone).toBe("077")
+            expect(searchResult.age).toBe("")
+
+            searchInput = "077)"
+            searchResult = parseSearchInfo(searchInput)
+            expect(searchResult.name).toBe("")
+            expect(searchResult.phone).toBe("077")
+            expect(searchResult.age).toBe("")
+
+            searchInput = "(077) 6-555"
+            searchResult = parseSearchInfo(searchInput)
+            expect(searchResult.name).toBe("")
+            expect(searchResult.phone).toBe("0776")
+            expect(searchResult.age).toBe("")
+
+            searchInput = "077 66-5-5"
+            searchResult = parseSearchInfo(searchInput)
+            expect(searchResult.name).toBe("")
+            expect(searchResult.phone).toBe("07766")
+            expect(searchResult.age).toBe("")
+
+            searchInput = "077 666-55"
+            searchResult = parseSearchInfo(searchInput)
+            expect(searchResult.name).toBe("")
+            expect(searchResult.phone).toBe("07766655")
+            expect(searchResult.age).toBe("")
+        })
+    })
+
+    describe("Entries with special char", () => {
+        test("Name * Phone", () => {
+            let searchInput = "Carlos * 055 7777777"
+            // The search of this test WILL give an error of SPECIAL_CHAR
+            expect(() => {parseSearchInfo(searchInput)}).toThrowError("SPECIAL_CHAR")
+        })
+    })
 });
